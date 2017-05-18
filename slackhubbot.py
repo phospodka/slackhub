226b04e109daf77e93c2fca38a0c4882931298ad
Peter Hospodka <phospodka@gmail.com>
2017-05-18 00:28:15 -0500

    Added simple get response to web hook

--- slackhubbot.py
+++ slackhubbot.py
@@ -88,6 +88,8 @@ def webhook_sink(token):
     """
     if settings.SLACKHUB_TOKEN != token or not settings.WEBHOOK_ENABLED:
         abort(403)
+    elif request.method == 'GET':
+        return 'Boop'
     # needs to handle trusted listening to known hosts and maybe just POST
     slackhub.webhooker.github_router(request.headers.environ['HTTP_X_GITHUB_EVENT'], request.json)
     return "OK"
